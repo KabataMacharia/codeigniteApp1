3aee6fc8ae14bd568a1685c984a896d9344e3996
tonygithinji <tonygithinji94@gmail.com>
2017-07-05 16:43:20 +0300

    Added google captcha and html reset password email

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,7 +6,7 @@ class Pages extends CI_Controller
 	public function __construct(){
 		parent::__construct();
 		if(!$this->session->userdata('user')){
-			//redirect('login');
+			redirect('login');
 		}
 	}
 
